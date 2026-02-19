6620a9dc8a115fe30a4ffe9010f5d26e02826038
Mikail Syed <mikailsyed@yahoo.com>
2026-02-19 12:45:07 -0500

    lol

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,6 +10,8 @@
 
 // remember to actually save the file here princess
 
+// i looooooooooooooooooooooooooooooooooooooooove his voice -- zain aamer
+
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
