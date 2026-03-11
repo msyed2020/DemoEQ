aaadf1cf5f287884fd3733468d80a152cf4600ae
Mikail Syed <mikailsyed@yahoo.com>
2026-03-11 08:48:58 -0400

    br

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,12 +10,6 @@
 
 // remember to actually save the file here princess
 
-// man i gotta work on my music shi haha
-
-// ok cool
-
-// i was a bit chill
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
