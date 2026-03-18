4c4bbb1e67c15bdf9d8e06be52c66582bf07a7a1
Mikail Syed <mikailsyed@yahoo.com>
2026-03-18 08:28:25 -0400

    b

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,12 +10,6 @@
 
 // remember to actually save the file here princess
 
-// if i got a story to tell i be tellin it
-
-// br
-
-// tweakin
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
